635f9f3fdbba2060badf0dec58a2a4b7e329dd64
alejandroem <alexalejandroem@gmail.com>
2018-12-12 19:42:56 +0100

    Added the layout for the cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+  key = '5aa543c21de894bf5b74e888f687fcee';
+  token = '31cad91d673e6962cfff97cbd615c027db63d237edbc9b83a4284c6618b198ab';
+  boardId = 'nC8QJJoZ';
+  apiUrl = `https://api.trello.com/1/boards/${this.boardId}/cards/?key=${this.key}&token=${this.token}`;
+
+  cards: any[] = [];
+  constructor(private client: HttpClient) {
+    console.log("Hi there human");
+  }
+
+
+  listTheCards() {
+    this.client.get(this.apiUrl)
+      .toPromise()
+      .then(result => {
+        console.log("Success ", result);
+      })
+      .catch(err => {
+        console.log("Error ", err);
+      });
+  }
 }
